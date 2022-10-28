e6290f5fa7eb510859a8af1b118d9ac275049217
lef237 <lef237k5@gmail.com>
2022-10-28 14:55:35 +0900

    toStringを削除した。eslintとprettierを通した。

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -3,13 +3,13 @@
 for (let i = 1; i <= 20; i++) {
   let result;
   if (i % 15 === 0) {
-    result = 'FizzBuzz';
+    result = "FizzBuzz";
   } else if (i % 3 === 0) {
-    result = 'Fizz';
+    result = "Fizz";
   } else if (i % 5 === 0) {
-    result = 'Buzz';
+    result = "Buzz";
   } else {
-    result = i.toString(10);
+    result = i;
   }
   console.log(result);
 }
