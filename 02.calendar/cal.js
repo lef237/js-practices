d1ea58fb1ccb615ef1516f89d3d0da7cc153040c
lef237 <lef237k5@gmail.com>
2022-11-01 11:19:01 +0000

    importを使って読み込めるようにした。ESmoduleを使う場合はこのように書く。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
 
-var argv = require('minimist')(process.argv.slice(2));
+// import endOfMonth from 'date-fns';
+import { endOfMonth } from 'date-fns'
+// let argv = require('minimist')(process.argv.slice(2));
+// var endOfMonth = require('date-fns/startOfMonth')
+
+import minimist from 'minimist';
+
+
+const argv = minimist(process.argv.slice(2))
+
+console.log(new Date('2000-01-01T00:00:00'))
+console.log(new Date(1999, 11, 31, 23, 59, 59, 999))
+
+const result = endOfMonth(new Date(2014, 8));
+console.log(result);
+
 
 let month = parseInt(argv.m) ? argv.m : 11;
 let year = parseInt(argv.y) ? argv.y : 2022;
+let first_day = 1;
 
-console.log(argv);
-console.log(month);
-console.log(year);
 
-process.stdout.write(`${month}`);
-
+console.log(`      ${month}月  ${year}`);
+console.log(`日 月 火 水 木 金 土`)
