c2f79a14e4c8d622339f7208097491b5c3bfcc89
lef237 <lef237k5@gmail.com>
2022-11-02 13:53:53 +0000

    とりあえずカレンダーのプログラムが出来た。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,27 +1,29 @@
 #!/usr/bin/env node
 
 let argv = require('minimist')(process.argv.slice(2));
-let lastDayOfMonth = require('date-fns/lastDayOfMonth')
-
-
-let month = parseInt(argv.m) ? argv.m -1 : 10;
-let year = parseInt(argv.y) ? argv.y : 2022;
-let target_date = new Date(year, month)
 let now_date = new Date()
+let month = argv.m - 1 || now_date.getMonth();
+let year = argv.y || now_date.getFullYear();
+let target_date = new Date(year, month);
+let lastDayOfMonth = require('date-fns/lastDayOfMonth')
+let lastday_of_target_date = lastDayOfMonth(target_date).getDate();
+let day_of_week_of_firstday = target_date.getDay()
+let initial_spaces = "  %".repeat(day_of_week_of_firstday)  //あとで7をday_of_week_of_firstdayに直す
+let isSaturday = require('date-fns/isSaturday')
 
-// debugger
+console.log(`      ${month + 1}月  ${year}`);
+console.log(` 日 月 火 水 木 金 土`);
 
-console.log(now_date)
-console.log(now_date.getMonth()+1)
+process.stdout.write(initial_spaces);
 
-console.log(target_date)
+// console.log(lastday_of_target_date);
 
-let result = lastDayOfMonth(target_date);
-console.log(result);
+for(let i = 1 ; i <= lastday_of_target_date; i++) {
+  let day = i.toString()
+  process.stdout.write(day.padStart(3))
+  // 改行する if 土曜日のとき
+ if (isSaturday(new Date(year, month , i))) {
+  process.stdout.write("\n")
+ }
+}
 
-
-let first_day = 1;
-
-
-console.log(`      ${month}月  ${year}`);
-console.log(`日 月 火 水 木 金 土`)
