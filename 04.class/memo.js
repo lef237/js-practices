29d68770b69e176eb10b5d9d8f8668ef63e44937
lef237 <lef237k5@gmail.com>
2022-11-13 21:23:59 +0900

    標準入力したものをJSONへと書き込めるようにした。reader.onの使い方を見直した。

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,32 +1,14 @@
 #!/usr/bin/env node
 
 import minimist from "minimist";
-// import * as readline from "node:readline";
 import * as readline from "node:readline";
-//fs/promisesモジュール全体を読み込む
 import * as fs from "node:fs";
-
 const argv = minimist(process.argv.slice(2));
-console.log(argv.l);
-console.log(argv.r);
-console.log(argv.d);
 
-// TODO:JSONファイルを読み込む→そのJSONファイルにデータを追加する→JSONファイルを上書きする
-
-// JSONファイルを読み込む
 let jsonFile = fs.readFileSync("memo.json", "utf8");
 let parsedJsonData = JSON.parse(jsonFile);
-console.log(parsedJsonData); // => []
 
-//※ここは配列じゃないとpushできない。pushを使わずに追加する方法は？→なさそう
-let new_data = { memo01: "memo01\nmemomemo" };
-parsedJsonData.push(new_data);
-console.log(parsedJsonData);
-
-// jsonファイルに書き込む
-let jsonedData = JSON.stringify(parsedJsonData);
-fs.writeFileSync("memo.json", jsonedData);
-
+process.stdin.resume();
 process.stdin.setEncoding("utf8");
 
 let lines = []; //ここに受け取ったテキストを保管する。改行ごとにカンマで区切られて配列になる
@@ -38,8 +20,21 @@ let reader = readline.createInterface({
 reader.on("line", (line) => {
   //改行ごとに"line"イベントが発火される
   lines.push(line); //ここで、lines配列に、標準入力から渡されたデータを入れる
+  console.log(lines);
 });
 reader.on("close", () => {
   //標準入力のストリームが終了すると呼ばれる
   console.log(`追加されました。: ${lines}`);
+  // TODO:joinを使って配列の中を連結し、\nを差し込む
+  console.log(lines);
+  let inputText = lines.join("\n");
+  console.log(inputText);
+
+  let new_data = { text: inputText };
+  parsedJsonData.push(new_data);
+  console.log(parsedJsonData);
+
+  // jsonファイルに書き込む
+  let jsonedData = JSON.stringify(parsedJsonData);
+  fs.writeFileSync("memo.json", jsonedData);
 });
