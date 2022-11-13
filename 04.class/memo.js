1987335d971031230f4a513a5042cc594ab2919d
lef237 <lef237k5@gmail.com>
2022-11-13 11:40:37 +0900

    echoとパイプで標準入力で渡された情報が配列の中に収まるようにした。改行文字も反映される。

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -6,12 +6,31 @@ console.log(argv.r);
 console.log(argv.d);
 console.log(argv);
 
-const readline = require("readline").createInterface({
+// const readline = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// readline.question("What is your name? ", (answer) => {
+//   console.log(`Hello, ${answer}!`);
+//   readline.close();
+// });
+
+// let aisatu = "こんんちは。\n今日は……";
+// console.log(`テスト, ${aisatu}`);
+
+process.stdin.setEncoding("utf8");
+
+let lines = [];
+let reader = require("readline").createInterface({
   input: process.stdin,
-  output: process.stdout,
 });
 
-readline.question("What is your name? ", (answer) => {
-  console.log(`Hello, ${answer}!`);
-  readline.close();
+reader.on("line", (line) => {
+  //改行ごとに"line"イベントが発火される
+  lines.push(line); //ここで、lines配列に、標準入力から渡されたデータを入れる
+});
+reader.on("close", () => {
+  //標準入力のストリームが終了すると呼ばれる
+  console.log(lines);
 });
