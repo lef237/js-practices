ed25635db4ad66a5fefa061e2fcd3451c3120e11
lef237 <lef237k5@gmail.com>
2022-11-13 22:36:15 +0900

    関数で処理をまとめて、if文で処理を条件分岐できるようにした。

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -5,36 +5,50 @@ import * as readline from "node:readline";
 import * as fs from "node:fs";
 const argv = minimist(process.argv.slice(2));
 
-let jsonFile = fs.readFileSync("memo.json", "utf8");
-let parsedJsonData = JSON.parse(jsonFile);
+console.log(argv.l);
+console.log(argv.r);
+console.log(argv.d);
 
-process.stdin.resume();
-process.stdin.setEncoding("utf8");
+//通常のデータ書き込み if argvが全てundefinedのときに実行する
+//ここの部分をファイル読み込み＆書き込みクラスにまとめられそう
+const addMemo = () => {
+  let jsonFile = fs.readFileSync("memo.json", "utf8");
+  let parsedJsonData = JSON.parse(jsonFile);
 
-let lines = []; //ここに受け取ったテキストを保管する。改行ごとにカンマで区切られて配列になる
-let reader = readline.createInterface({
-  input: process.stdin, //入力を受け取れるようになる
-  output: process.stdout, //ctrl+c後に結果が出力される
-});
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
 
-reader.on("line", (line) => {
-  //改行ごとに"line"イベントが発火される
-  lines.push(line); //ここで、lines配列に、標準入力から渡されたデータを入れる
-  console.log(lines);
-});
-reader.on("close", () => {
-  //標準入力のストリームが終了すると呼ばれる
-  console.log(`追加されました。: ${lines}`);
-  // TODO:joinを使って配列の中を連結し、\nを差し込む
-  console.log(lines);
-  let inputText = lines.join("\n");
-  console.log(inputText);
+  let reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  let lines = [];
+  reader.on("line", (line) => {
+    lines.push(line);
+  });
+  reader.on("close", () => {
+    //ここの部分もファイル読み込み＆書き込みクラスにまとめられそう
+    let inputText = lines.join("\n");
+    let new_data = { text: inputText };
+    parsedJsonData.push(new_data);
+    let jsonedData = JSON.stringify(parsedJsonData);
+    fs.writeFileSync("memo.json", jsonedData);
+    console.log(`メモが追加されました`);
+  });
+};
 
-  let new_data = { text: inputText };
-  parsedJsonData.push(new_data);
-  console.log(parsedJsonData);
+//オプションlで最初の行だけを表示する
+//この関数を実行する if argv.l
+const listMemos = () => {};
 
-  // jsonファイルに書き込む
-  let jsonedData = JSON.stringify(parsedJsonData);
-  fs.writeFileSync("memo.json", jsonedData);
-});
+if (argv.l) {
+  listMemos();
+} else if (argv.r) {
+  referenceMemos();
+} else if (argv.d) {
+  deleteMemo();
+} else {
+  addMemo();
+}
+
+// addMemo();
