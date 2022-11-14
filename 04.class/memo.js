390a14f08535ce55c248b7148fe53410d6d42f9d
lef237 <lef237k5@gmail.com>
2022-11-14 20:03:20 +0900

    rオプションをとりあえず実装した。

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-import minimist from "minimist";
-import * as readline from "node:readline";
-import * as fs from "node:fs";
-import enquirer from "enquirer";
+// "use strict";
 
-const argv = minimist(process.argv.slice(2));
+let argv = require("minimist")(process.argv.slice(2));
+const readline = require("node:readline");
+const fs = require("node:fs");
+
 console.log(argv.l);
 console.log(argv.r);
 console.log(argv.d);
@@ -64,18 +64,54 @@ const referenceMemos = () => {
   //   firstLineLists.push(splitedLines[0]);
   // });
   //この配列を使ってenquirerで選ばせる。選んだ選択肢の番号を取得する。
-  (async () => {
-    const question = {
-      type: "select",
-      name: "selectmemo",
-      message: "Choose a note you want to see:",
-      choices: ["バナナ", "チョコレート", "クッキー"],
-    };
-    const answer = await enquirer.prompt(question);
-    console.log(`${answer.selectmemo}が表示される`);
-  })();
+  // (async () => {
+  //   const question = {
+  //     type: "select",
+  //     name: "selectmemo",
+  //     message: "Choose a note you want to see:",
+  //     choices: ["バナナ", "チョコレート", "クッキー"],
+  //   };
+  //   const answer = await enquirer.prompt(question);
+  //   console.log(`${answer.selectmemo}が表示される`);
+  // })();
 
   //取得した番号を使ってその全文を表示させる。
+
+  // ("use strict");
+
+  // const colors = require("ansi-colors");
+  const { Select } = require("enquirer");
+
+  /**
+   * This prompt shows how you can easily customize the footer
+   * to render a different value based on conditions.
+   */
+
+  // let keys = Object.keys(colors.styles);
+  // let idx = 0;
+
+  console.log(parsedJsonData[0]);
+
+  const prompt = new Select({
+    name: "memos",
+    message: "Choose a note you want to see:",
+    // header: yosay("Welcome to my awesome generator!"),
+    footer() {
+      // let fn = colors[keys[++idx % keys.length]];
+      // return "\n" + fn("(Scroll up and down to reveal more choices)");
+      return "Scroll up and down to reveal more choices";
+    },
+    limit: 5,
+    choices: parsedJsonData,
+    result() {
+      return this.focused.text;
+    },
+  });
+
+  prompt
+    .run()
+    .then((text) => console.log(text))
+    .catch(console.error);
 };
 
 if (argv.l) {
